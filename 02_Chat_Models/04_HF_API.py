8122b8916821a00ce182d414bb7a7c93fa692e28
SakshamJr <jrsaksham10@gmail.com>
2026-02-06 18:03:15 +0545

    Fix

--- 02_Chat_Models/04_HF_API.py
+++
@@ -1,9 +0,0 @@
-from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
-
-llm = HuggingFaceEndpoint(
-    repo_id="Qwen/Qwen3-Coder-Next",
-    task="text-generation")
-
-model = ChatHuggingFace(llm=llm)
-result = model.invoke("Who is the prime minister of Nepal?")
-print(result.content)
